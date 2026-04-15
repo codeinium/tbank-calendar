f731df3186cbd09f6cf6230253471dd2a0f75608
myrshe <polina.zimnyackova@gmail.com>
2026-04-15 23:48:12 +0300

    add mobile sidebar

--- src/app/features/goals/pages/goals-page/goals-page.ts
+++ src/app/features/goals/pages/goals-page/goals-page.ts
@@ -1,16 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { GoalsSidebar } from '../../components/goals-sidebar/goals-sidebar'; 
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { GoalsSidebar } from '../../components/goals-sidebar/goals-sidebar';
 import { GoalsInfoContainer } from '../../components/goals-info-container/goals-info-container';
 import { GoalsPageStore } from '../../services/goal.service';
+import { TuiButton } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-goals-page',
-  imports: [ GoalsSidebar, GoalsInfoContainer],
+  imports: [GoalsSidebar, GoalsInfoContainer, TuiButton],
   templateUrl: './goals-page.html',
   styleUrl: './goals-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [GoalsPageStore]
+  providers: [GoalsPageStore],
 })
 export class GoalsPageComponent {
-  
+  isSidebarOpen = signal(false);
 }
