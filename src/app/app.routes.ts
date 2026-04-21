b4964fd02234d8c055b15b4b09a142b343a9449e
myrshe <polina.zimnyackova@gmail.com>
2026-04-21 22:01:57 +0300

    add not-found-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout';
+import { ErrorLayout } from './layouts/error-layout/error-layout';
 
 
 export const routes: Routes = [
@@ -29,10 +30,25 @@ export const routes: Routes = [
       {
         path: 'goals',
         loadComponent: () =>
-          import('./features/goals/pages/goals-page/goals-page').then(
-            (m) => m.GoalsPageComponent,
+          import('./features/goals/pages/goals-page/goals-page').then((m) => m.GoalsPageComponent),
+      },
+    ],
+  },
+  {
+    path: '',
+    component: ErrorLayout,
+    children: [
+      {
+        path: 'not-found',
+        loadComponent: () =>
+          import('./features/not-found/pages/not-found-page/not-found-page').then(
+            (m) => m.NotFoundPage,
           ),
       },
     ],
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
   },
 ];
