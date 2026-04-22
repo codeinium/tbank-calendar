b9def4044a969d0fdc7c3bdcc17cf1e7752cb7a6
myrshe <polina.zimnyackova@gmail.com>
2026-04-23 02:08:19 +0300

    add skeleton info container

--- src/app/shared/components/skeleton-block/skeleton-block.ts
+++ src/app/shared/components/skeleton-block/skeleton-block.ts
@@ -4,11 +4,21 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-skeleton-block',
   template: `
-    <div class="bg-(--loading-1) rounded-(--tui-radius-l)" [ngClass]="height + ' ' + width"></div>
+    <div class="bg-(--loading-1) rounded-(--tui-radius-l) flex flex-col" [ngClass]="classes">
+      <ng-content />
+    </div>
   `,
-  imports: [NgClass]
+  imports: [NgClass],
 })
 export class SkeletonBlock {
   @Input() width = '';
   @Input() height = '';
+
+  @Input() px: string = 'px-0';
+  @Input() py: string = 'py-0';
+  @Input() gap: string = 'gap-0';
+
+  get classes() {
+    return [this.width, this.height, this.px, this.py, this.gap].filter(Boolean).join(' ');
+  }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,5 +5,5 @@ export const environment: EnvironmentConfig = {
     apiUrl: '/api/v1',
 
     useMock: true,
-    mockDelay: 1000,
+    mockDelay: 3000,
 };
